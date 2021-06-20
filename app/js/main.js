be9fe00c46b5c302a86a8af958da6248021d6f66
Matrosken <piranya1399@gmail.com>
2021-06-20 20:12:25 +0400

    fefe

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,10 @@
 $( document ).ready(function() {
-  var swiper = new Swiper('.about_container', {
-    slidesPerView: 'auto',
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      hide: false,
-      draggable: true,
-      snapOnRelease: false,
-    },
-  });
+    var swiper = new Swiper('.banner_swiper', {
+        slidesPerView: '1',
+        
+    });
+    var swiper = new Swiper('.review_swiper', {
+        slidesPerView: '1',
+        
+    });
 });
